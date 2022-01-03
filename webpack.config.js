10c59a12997dad7e2d88bc1ccb62850904ae4cc7
sina alizadeh <sina.alizadeh.120@gmail.com>
2022-01-03 12:50:27 +0330

    add css scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
+  target = "browserlist";
   mode = "production";
 }
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: mode,
+  target: target,
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -17,8 +30,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
   devtool: "source-map",
   devServer: {
     static: "./dist",
+    hot: true,
   },
 };
